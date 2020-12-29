b3814335ae58370a3db8c826708a82a983bde245
Bart Bakker <bart@thebigtrip.nl>
2020-12-29 21:19:31 +0100

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t d0ckd0ck/multi-client:latest -t d0ckd0ck/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t d0ckd0ck/multi-server:latest -t d0ckd0ck/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t d0ckd0ck/multi-worker:latest -t d0ckd0ck/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push d0ckd0ck/multi-client:latest
+docker push d0ckd0ck/multi-server:latest
+docker push d0ckd0ck/multi-worker:latest
+
+docker push d0ckd0ck/multi-client:$SHA
+docker push d0ckd0ck/multi-server:$SHA
+docker push d0ckd0ck/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=d0ckd0ck/multi-server:$SHA
+kubectl set image deployments/client-deployment server=d0ckd0ck/client-server:$SHA
+kubectl set image deployments/worker-deployment server=d0ckd0ck/worker-server:$SHA
